b0e2a3950cc7b9a619346b8f06465fbe099b54c8
wangsir <15713912912.163.com>
2021-11-05 11:38:51 +0800

    修改javaScript链接地址出错问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,7 +50,7 @@ export default {
           },
           {
             text: 'JavaScript',
-            link: 'https://wangsirxy.github.io/JavaScript/',
+            link: 'https://WangSirXY.github.io/javaScriptCourse',
             activeMatch: '^/config/',
           },
           {
